d7d9cad84d230264f37f71ddfaf52ab621185488
vasilikisv <vasiliki_sv@hotmail.com>
2015-04-22 14:27:37 +0300

    initiate myapp

--- energagement/energagement/urls.py
+++ energagement/energagement/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('allauth.urls')),
+    url(r'^myapp/', include('myapp.urls'))
 
 )
---
+++ energagement/myapp/urls.py
@@ -0,0 +1,9 @@
+__author__ = 'vasiliki'
+
+from django.conf.urls import patterns, url
+
+from myapp import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.main, name='main'),
+)
