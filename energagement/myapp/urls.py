72059550ad6602d78c4c98ab1601fab9ea38bd27
vasilikisv <vasiliki_sv@hotmail.com>
2015-05-16 23:33:54 +0300

    edit divs in main, link login with main

--- energagement/myapp/urls.py
+++ energagement/myapp/urls.py
@@ -6,4 +6,7 @@ from myapp import views
 
 urlpatterns = patterns('',
     url(r'^main/', views.main, name='main'),
+    url(r'^buildings/', views.buildings, name='buildings'),
+    url(r'^street_lighting/', views.street_lighting, name='street_lighting'),
+    url(r'^EV/', views.EV, name='EV'),
 )
--- energagement/myapp/views.py
+++ energagement/myapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render_to_response
 def main(request):
 
     return render_to_response('myapp/main.html')
+
+def buildings(request):
+
+    return render_to_response('myapp/buildings.html')
+
+def street_lighting(request):
+
+    return render_to_response('myapp/street_lighting.html')
+
+def EV(request):
+
+    return render_to_response('myapp/EV.html')
