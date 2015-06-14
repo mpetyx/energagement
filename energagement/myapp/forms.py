21a502fbb8118c1e9ca9ef2bfaa0f03db2c4370b
vasilikisv <vasiliki_sv@hotmail.com>
2015-06-15 01:39:29 +0300

    change EV for "presentation", add dropdown form for choosing time

--- energagement/myapp/forms.py
+++ energagement/myapp/forms.py
@@ -14,5 +14,25 @@ from django import forms
 #         )
 #     )
 
-class myForm(forms.Form):
-    status = forms.BooleanField(widget=forms.CheckboxInput(attrs={'onclick':"alert('foo !');"}),required=True, label="Status")
+#attrs={'onclick':"alert('foo !');"}),
+
+class myForm1(forms.Form):
+    EV1 = forms.BooleanField(widget=forms.CheckboxInput(attrs={}),required=True, label="EV1")
+
+class myForm2(forms.Form):
+    EV2 = forms.BooleanField(widget=forms.CheckboxInput(attrs={}),required=True, label="EV2")
+
+class myForm3(forms.Form):
+    EV3 = forms.BooleanField(widget=forms.CheckboxInput(attrs={}),required=True, label="EV3")
+
+class myForm4(forms.Form):
+    kW = forms.BooleanField(widget=forms.CheckboxInput(attrs={}),required=True, label="kW")
+
+class myForm5(forms.Form):
+    kW_km = forms.BooleanField(widget=forms.CheckboxInput(attrs={}),required=True, label="kW/km")
+
+class myForm6(forms.Form):
+    Euro_km = forms.BooleanField(widget=forms.CheckboxInput(attrs={}),required=True, label="Euro/km")
+
+class my_choose_time(forms.Form):
+    choose_time = forms.ChoiceField(choices=[(x, x) for x in ('hour', 'day', 'week', 'month')])
--- energagement/myapp/views.py
+++ energagement/myapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render_to_response
 from django.shortcuts import render
-from .forms import myForm
+from .forms import myForm1
+from .forms import myForm2
+from .forms import myForm3
+from .forms import myForm4
+from .forms import myForm5
+from .forms import myForm6
+from .forms import my_choose_time
+
 from django.http import HttpResponseRedirect
 import cgi
 
@@ -26,22 +33,34 @@ def EV(request):
     if request.method == 'GET':
         diagram = None
         # create a form instance and populate it with data from the request:
-        form = myForm(request.GET)
+        form1 = myForm1(request.GET)
+        form2 = myForm2(request.GET)
+        form3 = myForm3(request.GET)
+        form4 = myForm4(request.GET)
+        form5 = myForm5(request.GET)
+        form6 = myForm6(request.GET)
+        choose_time = my_choose_time(request.GET)
         # check whether it's valid:
-        if form.is_valid():
+        #if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-        return render(request, 'myapp/EV.html', {'form':form, 'diagram':diagram})
+          #  return HttpResponseRedirect('/thanks/')
+        return render(request, 'myapp/EV.html', {'form1':form1,'form2':form2,'form3':form3,'form4':form4,'form5':form5,'form6':form6, 'choose_time':choose_time, 'diagram':diagram})
     #form=myForm()
     # if a GET (or any other method) we'll create a blank form
     else:
         # form1 = cgi.FieldStorage()
         # diagram = form1.getvalue('diagram')
         diagram = request.POST.get("diagram","")
-        form = myForm()
-        return render(request, 'myapp/EV.html', {'form':form, 'diagram':diagram},)
+        form1 = myForm1()
+        form2 = myForm2()
+        form3 = myForm3()
+        form4 = myForm4()
+        form5 = myForm5()
+        form6 = myForm6()
+        choose_time = my_choose_time()
+        return render(request, 'myapp/EV.html', {'form1':form1, 'form2':form2, 'form3':form3,'form4':form4,'form5':form5,'form6':form6,'choose_time':choose_time,'diagram':diagram},)
 
 
 def test(request):
